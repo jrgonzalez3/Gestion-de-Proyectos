abb94224cf6940f1650cd36100727499331f12c8
Justo Ramon Gonzalez Chavez <jrgonzalez3@gmail.com>
2020-08-25 20:52:01 -0400

    Update Conexion.php
    ok

--- admin/config/Conexion.php
+++ admin/config/Conexion.php
@@ -1,15 +1,29 @@
 <?php 
-require_once "global.php";
+//require_once "global.php";
 
-$conexion=new mysqli(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
-
-mysqli_query($conexion, 'SET NAMES "'.DB_ENCODE.'"');
+//$conexion=new mysqli(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
+//$conexion=new mysqli("mysql311.eshos.com","adad","adasd","asdasda");
+//mysqli_query($conexion, 'SET NAMES "'.DB_ENCODE.'"');
 
 //muestra posible error en la conexion
-if (mysqli_connect_errno()) {
-	printf("Ups parece que falló en la conexion con la base de datos: %s\n",mysqli_connect_error());
-	exit();
+//if (mysqli_connect_errno()) {
+//	printf("Ups parece que falló en la conexion con la base de datos: %s\n",mysqli_connect_error());
+//	exit();
+//}
+
+setlocale(LC_TIME, "es_ES.UTF-8");
+$conexion = @mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+mysqli_query($con, "SET NAMES UTF8");
+
+if (!$conexion) {
+    die("Imposible conectarse!: " . mysqli_error($conexion));
 }
+if (@mysqli_connect_errno()) {
+    die("La Conexión falló: " . mysqli_connect_errno() . " : " . mysqli_connect_error());
+}
+
+
+
 
 if (!function_exists('ejecutarConsulta')) {
 	Function ejecutarConsulta($sql){ 
